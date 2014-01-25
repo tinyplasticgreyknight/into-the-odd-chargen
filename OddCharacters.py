9d189264a664eaf32f600efd88a59a125cc71832
GreyKnight <tinyplasticgreyknight@yahoo.com>
2014-01-25 13:57:17 +0000

    add README don't give all characters a starting arcanum; only those who have high Will!

--- OddCharacters.py
+++ OddCharacters.py
@@ -7,7 +7,7 @@ def main(n=1):
     n = int(n)
     chars = []
     for _ in range(n):
-        chars.append( Character(force_arcanum=True) )
+        chars.append( Character() )
     print yaml.dump_all(chars, default_flow_style=False)
 
 if __name__=='__main__':
